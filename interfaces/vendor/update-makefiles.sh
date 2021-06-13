a8efcf5fe5e46a58d9944cf9095eed67e5fea1a5
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2021-06-13 19:03:07 +0000

    RMX1971: oppo -> oplus
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- interfaces/vendor/update-makefiles.sh
+++ interfaces/vendor/update-makefiles.sh
@@ -3,4 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.oppo:device/realme/RMX1971/interfaces/vendor"
+  "vendor.oplus:device/realme/RMX1971/interfaces/vendor"
