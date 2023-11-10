ed8fc30e033fc837a247f7aa72bf8deb7746fdff
kaderbava <ksbava7325@gmail.com>
2023-11-10 20:19:31 +0000

    Revert "RMX1921: DEXPREOPT SettingsGoogle and SystemUIGoogle"
    This reverts commit 5f9dfc58d70cca060e8619545c2b502c72fc15ad.

--- device.mk
+++ device.mk
@@ -157,8 +157,7 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SettingsGoogle \
-    SystemUIGoogle
+    SystemUI
 
 # DRM
 PRODUCT_PACKAGES += \
