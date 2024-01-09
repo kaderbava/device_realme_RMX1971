d58172d9ec3217cb28501e6cbe5e4defe8b17291
kaderbava <ksbava7325@gmail.com>
2024-01-09 20:26:16 +0000

    RMX1971: Adapt to pixel-framework
    * This reverts commit 490134d ed8fc30.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -157,7 +157,8 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SettingsGoogle \
+    SystemUIGoogle
 
 # DRM
 PRODUCT_PACKAGES += \
