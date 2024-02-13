f4e4e0bb02ed233f292d383c2b7591440d27a225
kaderbava <ksbava7325@gmail.com>
2024-02-13 22:29:43 +0000

    RMX1971: Switch to mini gapps
    * Also adapt to new gms.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos SoundAmplifierPrebuilt GoogleCamera AdaptiveVPNPrebuilt CalendarGooglePrebuilt DevicePolicyPrebuilt FilesPrebuilt ScribePrebuilt PrebuiltGmail Maps YouTube
+LOCAL_OVERRIDES_PACKAGES := GoogleCamera AdaptiveVPNPrebuilt CalendarGooglePrebuilt FilesPrebuilt PlayAutoInstallConfig SafetyHubPrebuilt talkback Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
