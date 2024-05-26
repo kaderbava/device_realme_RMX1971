1309f079753f3358406343ccca8141e692e4ee14
kaderbava <ksbava7325@gmail.com>
2024-05-26 22:37:42 +0000

    RMX1971: Disable blurs explicitly
    * Better do this in device side.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- derp_RMX1971.mk
+++ derp_RMX1971.mk
@@ -6,7 +6,6 @@
 
 # Inherit some common DerpFest stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_NOT_USES_BLUR := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_CALL_RECORDING := true
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
