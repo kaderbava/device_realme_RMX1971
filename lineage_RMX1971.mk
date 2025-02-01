93497f8daf7e57085b649f2ad2bee8c7bdb2949c
kaderbava <ksbava7325@gmail.com>
2025-02-01 22:37:15 +0000

    RMX1971: Matrixx-ify!
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- lineage_RMX1971.mk
+++ lineage_RMX1971.mk
@@ -12,6 +12,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from RMX1971 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Matrixx
+MATRIXX_CHIPSET := SDM712
+MATRIXX_BATTERY := 4000mAh
+MATRIXX_DISPLAY := 1080x2340
+
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1971
 PRODUCT_MANUFACTURER := realme
