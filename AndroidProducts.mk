9fcffd4cdc170ca5f09200f34c5d37e0bd2ab36d
kaderbava <ksbava7325@gmail.com>
2025-05-18 23:50:33 +0000

    RMX1971: Adapt for DerpFest 15.2
    * DerpFest moved to lineageOS base. So adapt dt for it.
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_RMX1971.mk
+    $(LOCAL_DIR)/lineage_RMX1971.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_RMX1971-userdebug \
-    derp_RMX1971-eng
+    lineage_RMX1971-user \
+    lineage_RMX1971-userdebug \
+    lineage_RMX1971-eng
--- lineage_RMX1971.mk
+++ lineage_RMX1971.mk
@@ -1,23 +1,24 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit some common DerpFest stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Official-ify
-DERP_BUILDTYPE := Official
+DERPFEST_BUILD_TYPE := Official
+DERPFEST_BUILD_VARIANT := Stable
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1971
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := derp_RMX1971
+PRODUCT_NAME := lineage_RMX1971
 PRODUCT_MODEL := RMX1971
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
