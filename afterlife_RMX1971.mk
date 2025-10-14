c03eccef4f1ae4cd01150919853e4d26e2caccb2
kaderbava <ksbava7325@gmail.com>
2025-10-14 22:59:22 +0000

    RMX1971: AfterlifeOS bringup!
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- afterlife_RMX1971.mk
+++ afterlife_RMX1971.mk
@@ -4,20 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Pixelage stuff
+# Inherit some common AfterlifeOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-PIXELAGE_MAINTAINER := KaderBava
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Official-ify
+AFTERLIFE_MAINTAINER := Bava7325
+
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1971
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := pixelage_RMX1971
+PRODUCT_NAME := afterlife_RMX1971
 PRODUCT_MODEL := RMX1971
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
