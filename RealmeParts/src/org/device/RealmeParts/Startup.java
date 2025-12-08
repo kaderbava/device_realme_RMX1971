249c80dbc2108aa514ff83c509472d36fe582776
Bruno Nartins <bgcngm@gmail.com>
2025-12-08 14:32:41 +0600

    RMX1971: Replace Activity.MODE_PRIVATE with Context.MODE_PRIVATE
    Activity.MODE_PRIVATE is redundant because Activity is already a Context.
    
    Change-Id: Ib2e751de93294efe251fd5af272be27438b50025
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- RealmeParts/src/org/device/RealmeParts/Startup.java
+++ RealmeParts/src/org/device/RealmeParts/Startup.java
@@ -17,7 +17,6 @@
 */
 package org.device.RealmeParts;
 
-import android.app.Activity;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.content.BroadcastReceiver;
@@ -66,7 +65,7 @@ public class Startup extends BroadcastReceiver {
 
         enableComponent(context, ScreenOffGesture.class.getName());
         SharedPreferences screenOffGestureSharedPreferences = context.getSharedPreferences(
-                Utils.PREFERENCES, Activity.MODE_PRIVATE);
+                Utils.PREFERENCES, Context.MODE_PRIVATE);
         KernelControl.enableGestures(
                 screenOffGestureSharedPreferences.getBoolean(
                         ScreenOffGesture.PREF_GESTURE_ENABLE, false));
