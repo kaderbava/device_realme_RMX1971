6060b88560a7f9cb28a07a7e6697ca39e2e569ff
kaderbava <ksbava7325@gmail.com>
2026-05-23 17:54:31 +0000

    Revert "RMX1971: Sign my builds with releasekeys"
    This reverts commit f77e7b19f8137f78f1cd1e1bf97d7b1e56789dba.

--- lineage_RMX1971.mk
+++ lineage_RMX1971.mk
@@ -45,6 +45,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=RMX1971 \
     SystemDevice=RMX1971 \
     SystemName=RMX1971
-
-# Sign builds
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
