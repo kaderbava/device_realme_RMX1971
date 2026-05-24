d1c1ba40c63e42904d39d6f3befbf1c791998538
kaderbava <ksbava7325@gmail.com>
2026-05-24 16:57:42 +0000

    RMX1971: Matrixx A16 bringup!
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2026 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1971.mk
+    $(LOCAL_DIR)/matrixx_RMX1971.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1971-userdebug \
-    lineage_RMX1971-eng
+    matrixx_RMX1971-user \
+    matrixx_RMX1971-userdebug \
+    matrixx_RMX1971-eng
